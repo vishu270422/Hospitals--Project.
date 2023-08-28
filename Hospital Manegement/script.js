9cec28ae97a880e604b955556a898fedf69afb6a
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2023-08-28 20:37:42 +0530

    script.js

--- Hospital Manegement/script.js
+++ Hospital Manegement/script.js
@@ -1,9 +1,1 @@
-const navbarShowBtn = document.querySelector('.navbar-show-btn');
-
-navbarShowBtn.addEventListener('click', function(){
-    navbarCollapseDiv.classList.add('navbar-show');
-});
-navbarHideBtn.addEventListener('click', function(){
-    navbarCollapseDiv.classList.remove('navbar-show');
-});
 

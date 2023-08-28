0b31c678ea2b23834ec959d65fa9977ff07db75f
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2023-08-28 21:02:22 +0530

    script.js

--- Hospital Manegement/script.js
+++ Hospital Manegement/script.js
@@ -1,1 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const learnMoreButton = document.querySelector(".btn.btn-white");
+    if (learnMoreButton) {
+        learnMoreButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            alert("Learn More button clicked!");
+            
+        });
+    }
+
+ 
+    const form = document.querySelector("form");
+    if (form) {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+      
+            const nameInput = document.querySelector('input[name="name"]');
+            const emailInput = document.querySelector('input[name="email"]');
+            const messageTextarea = document.querySelector('textarea[name="message"]');
+
+            const name = nameInput.value;
+            const email = emailInput.value;
+            const message = messageTextarea.value;
+
+
+            console.log("Name: " + name);
+            console.log("Email: " + email);
+            console.log("Message: " + message);
+
+            nameInput.value = "";
+            emailInput.value = "";
+            messageTextarea.value = "";
+
+            alert("Message sent successfully!");
+        });
+    }
+});
 
